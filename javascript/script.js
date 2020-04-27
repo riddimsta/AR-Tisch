c99f75eecacce4ed37a0e74320eb0b7a5f159798
riddimsta <chr.felder@web.de>
2020-04-27 11:59:06 +0200

    index.html

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,10 @@
 var scene, camera, renderer, clock, deltaTime, totalTime;
-
 var arToolkitSource, arToolkitContext;
-
 var markerRoot1;
 var objects;
 var model;
-var mesh1;
-var neuesmesh;
-
-var mouse;
-
-raycaster = new THREE.Raycaster();
-mouse = new THREE.Vector2();
 
 initialize();
-
 
 function initialize() {
 
@@ -27,9 +17,6 @@ function initialize() {
     var dirLight = new THREE.DirectionalLight(0xffffff);
     dirLight.position.set(75, 300, -75);
     scene.add(dirLight);
-
-
-    camera = new THREE.Camera();
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 
@@ -49,7 +36,6 @@ function initialize() {
     renderer.domElement.style.left = '0px'
     renderer.gammaOutput = true;
     renderer.gammaFactor = 2;
-    //renderer.physicallyCorrectLights=true;
 
     document.body.appendChild(renderer.domElement);
 
@@ -113,71 +99,23 @@ function initialize() {
     markerRoot1 = new THREE.Group();
     scene.add(markerRoot1);
     let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-        type: 'pattern', patternUrl: "pattern-marker.patt", smooth: 'true',
+        type: 'pattern', patternUrl: "pattern-marker1.patt", smooth: 'true',
     })
 
-
-    let material1 = new THREE.MeshNormalMaterial({
-        transparent: true,
-        opacity: true,
-        side: THREE.DoubleSide
-    });
-    const radius = 6;
-    const height = 8;
-    const segments = 16;
-    const geometry = new THREE.ConeBufferGeometry(radius, height, segments);
-    mesh1 = new THREE.Mesh(geometry, material1);
-    mesh1.scale.set(0.04, 0.04, 0.04);
-    mesh1.position.y = 1.7;
-    mesh1.rotation.x = Math.PI;
-    mesh1.name = "0";
-    mesh1.visible = false;
-
-
-    mesh2 = new THREE.Mesh(geometry, material1);
-    mesh2.scale.set(0.04, 0.04, 0.04);
-    mesh2.position.y = 1.7;
-    mesh2.position.x = 1.2;
-    mesh2.position.z = 1.2;
-    mesh2.rotation.x = Math.PI;
-    mesh2.name = "1";
-    mesh2.visible = false;
-
-    objects = new THREE.Object3D();
-    objects.add(mesh1);
-    objects.add(mesh2);
-
-    markerRoot1.add(objects);
     var manager = new THREE.LoadingManager();
 
     var loader = new THREE.GLTFLoader(manager);
 
-
-    //var dracoLoader = new THREE.DRACOLoader();
-    //dracoLoader.setDecoderPath( '/js/threejs/libs/draco/' );
-    //loader.setDRACOLoader( dracoLoader );
-
-
-    var filename = "assets/lidice_model.glb";
+    var filename = "assets/images/lidice_model.glb";
 
     loader.load(filename, function (s) {
 
         model = s.scene;
         console.log("model loaded");
-
-            //traverse gltf scene content
-
-            //var model = object.getObjectByName('Cube');
-            //    var control = new TransformControls(camera, renderer.domElement);
-
-            console.log("--> traversing gltf scene");
+        console.log("--> traversing gltf scene");
         var index = 0;
         model.traverse(function (child) {
             markerRoot1.add.child;
-
-            // control.attach(child);
-
-
             console.log(index + " - " + child.name);
             index++;
             if (child.name == "#1" || child.name == "#2" || child.name == "#3" || child.name == "#4" ||
@@ -194,9 +132,6 @@ function initialize() {
 
         animate();
 
-
-        //  renderer.render( scene, camera );
-
     });
 }
 
@@ -205,34 +140,33 @@ function update() {
     if (arToolkitSource.ready !== false)
         arToolkitContext.update(arToolkitSource.domElement);
 
-
+    model.traverse(function (child) {
+        if (child.name == "#1" || child.name == "#2" || child.name == "#3" || child.name == "#4" ||
+            child.name == "#5" || child.name == "#6" || child.name == "#7" || child.name == "#8") {
+            if (child.visible) {
+                // child.rotation.y += 0.1;
+/*                while (child.position.y < 0.05) {
+                    child.position.y += 0.0001;
+                    //obj.rotateOnAxis(new THREE.Vector3(0,0,1), 90*Math.PI/180);
+                }*/
+            }
+        }
+    });
 }
 
 
 function render() {
     requestAnimationFrame(render);
-
-
 }
 
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    /*        model.traverse(function (child) {
-                if (child.name == "0#" || child.name == "1#" || child.name == "2#" || child.name == "3#" ||
-                child.name == "4#" || child.name == "5#" || child.name == "6#" || child.name == "7#") {
-                    if (child.visible) {
-                        child.rotation.y += 1;
-                        obj.rotateOnAxis(new THREE.Vector3(0,0,1), 90*Math.PI/180);
-                    }
-                }
-            });*/
+
     update();
     render();
-
 }
-
 
 $(function () {
     var icons = {
@@ -264,12 +198,11 @@ $(function () {
 });
 
 function removeObject() {
-
     model.traverse(function (child) {
         if (child.name == "#1" || child.name == "#2" || child.name == "#3" || child.name == "#4" ||
             child.name == "#5" || child.name == "#6" || child.name == "#7" || child.name == "#8") {
             child.visible = false;
-        };
+        }
+        ;
     });
-
 }
